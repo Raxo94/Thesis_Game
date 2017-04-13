13c3e7f41cdc9bdc75404dad2e28d045f131d869
BTH <BTH>
2017-04-13 15:32:47 +0200

    Made basic collision and camera system

---
+++ New Unity Project/Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour {
+    int count;
+    public movement player;
+
+    // Use this for initialization
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player" && gameObject != player.latestCollisionObject)
+        {
+            player.latestCollisionObject = gameObject;
+            player.collisionCount++;
+            print(player.collisionCount);
+            
+
+
+        }
+    }
+}
--- New Unity Project/Assets/Scripts/movement.cs
+++ New Unity Project/Assets/Scripts/movement.cs
@@ -7,16 +7,20 @@ public class movement : MonoBehaviour {
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
+
+    public GameObject latestCollisionObject;
+    public int collisionCount;
     // Use this for initialization
     void Start (){
-   }
+        collisionCount = 0;
+}
 	
 	// Update is called once per frame
 	void Update() {
         CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); 
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
             if (Input.GetButton("Jump"))
