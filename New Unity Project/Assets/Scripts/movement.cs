c8e663fbfc886183b3b26f42b86eaf2a08a9b0e7
Raxo94 <Oskar_swing@hotmail.com>
2017-05-07 11:21:49 +0200

    a

--- New Unity Project/Assets/Scripts/movement.cs
+++ New Unity Project/Assets/Scripts/movement.cs
@@ -7,17 +7,20 @@ public class movement : MonoBehaviour {
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
+    CharacterController controller;
 
     public GameObject latestCollisionObject;
     public int collisionCount;
     // Use this for initialization
-    void Start (){
+    void Start ()
+    {
         collisionCount = 0;
-}
+        controller = GetComponent<CharacterController>();
+    }
 	
 	// Update is called once per frame
 	void Update() {
-        CharacterController controller = GetComponent<CharacterController>();
+        
         if (controller.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); 
@@ -25,7 +28,6 @@ public class movement : MonoBehaviour {
             moveDirection *= speed;
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
-
         }
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
